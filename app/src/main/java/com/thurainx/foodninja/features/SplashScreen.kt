f7d19629c462b816fdab2342dcc4ed8b366af6a9
thurain <thurainbowin.dev@gmail.com>
2023-02-02 19:53:39 +0630

    Add Food Pattern Background
    - add dimen
    - refector code

--- app/src/main/java/com/thurainx/foodninja/features/SplashScreen.kt
+++ app/src/main/java/com/thurainx/foodninja/features/SplashScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
@@ -19,8 +20,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.thurainx.foodninja.R
-import com.thurainx.foodninja.ui.theme.Green
-import com.thurainx.foodninja.ui.theme.Viga
+import com.thurainx.foodninja.ui.theme.*
 
 
 @Preview(showBackground = true)
@@ -38,12 +38,46 @@ fun SplashScreen() {
             Image(
                 painterResource(id = R.drawable.app_logo),
                 "",
-                modifier = Modifier.size(width = 200.dp, height = 200.dp)
+                modifier = Modifier.size(width = SIZE_APP_LOGO, height = SIZE_APP_LOGO)
             )
-            Text(text = "FoodNinja", style = TextStyle(color = Green, fontSize = 32.sp, fontFamily = Viga))
-            Text(text = "Deliver Favorite Food", style = TextStyle(color = MaterialTheme.colors.onPrimary, fontSize = 14.sp, fontWeight = FontWeight.Bold))
+            Text(
+                text = "FoodNinja",
+                style = TextStyle(color = Green, fontSize = TEXT_HEADING_2X, fontFamily = Viga)
+            )
+            Text(
+                text = "Deliver Favorite Food",
+                style = TextStyle(
+                    color = MaterialTheme.colors.onPrimary,
+                    fontSize = TEXT_REGULAR,
+                    fontWeight = FontWeight.Bold
+                )
+            )
 
         }
+        BackgroundFoodPattern()
+
     }
+
+}
+
+@Composable
+fun BackgroundFoodPattern() {
+    Image(
+        painter = painterResource(id = R.drawable.food_pattern),
+        contentDescription = "",
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(320.dp)
+    )
+    Box(
+        modifier = Modifier.fillMaxWidth().height(320.dp).background(
+            brush = Brush.verticalGradient(
+                colors = listOf(
+                    Color.Transparent,
+                    MaterialTheme.colors.background
+                )
+            )
+        )
+    )
 
 }
---
+++ app/src/main/java/com/thurainx/foodninja/ui/theme/Dimen.kt
@@ -0,0 +1,32 @@
+package com.thurainx.foodninja.ui.theme
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+//Generic
+val MARGIN_SMALLER = 2.dp
+val MARGIN_SMALL = 4.dp
+val MARGIN_CARD_MEDIUM = 6.dp
+val MARGIN_MEDIUM = 8.dp
+val MARGIN_CARD_MEDIUM_2 = 12.dp
+val MARGIN_MEDIUM_2 = 16.dp
+val MARGIN_CARD_MEDIUM_3 = 18.dp
+val MARGIN_MEDIUM_3 = 20.dp
+val MARGIN_LARGE = 24.dp
+val MARGIN_XLARGE = 32.dp
+val MARGIN_XXLARGE = 48.dp
+
+val TEXT_SMALLER = 10.sp
+val TEXT_SMALL = 12.sp
+val TEXT_REGULAR = 14.sp
+val TEXT_REGULAR_2X = 16.sp
+val TEXT_REGULAR_2X_medium = 18.sp
+val TEXT_REGULAR_3X = 20.sp
+val TEXT_HEADING_1X = 24.sp
+val TEXT_HEADING_2X = 32.sp
+val TEXT_BIG = 40.sp
+val TEXT_BIG_2X = 60.sp
+val TEXT_BIG_3X = 80.sp
+
+// App Specific
+val SIZE_APP_LOGO = 200.dp
